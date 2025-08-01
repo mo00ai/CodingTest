66a02853650c8af5e73f3b89cfb602a6a8067512
AhyunCho <jah112482@gmail.com>
2025-08-01 23:48:20 +0900

    init : project

---
+++ src/backjoon/P18108.java
@@ -0,0 +1,15 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class P18108 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int thaiBirth = sc.nextInt();
+		int koreaBirth = thaiBirth - 543;
+		System.out.println(koreaBirth);
+
+	}
+}
