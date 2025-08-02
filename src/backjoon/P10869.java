a57166dfb79b24dfde1cf7da45b9981e6e5db0f0
AhyunCho <jah112482@gmail.com>
2025-08-02 16:38:17 +0900

    feat : 10869

---
+++ src/backjoon/P10869.java
@@ -0,0 +1,17 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class P10869 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int firstNum = sc.nextInt();
+		int secondNum = sc.nextInt();
+
+		System.out.println(firstNum + secondNum);
+		System.out.println(firstNum - secondNum);
+		System.out.println(firstNum * secondNum);
+		System.out.println(firstNum / secondNum);
+		System.out.println(firstNum % secondNum);
+	}
+}
