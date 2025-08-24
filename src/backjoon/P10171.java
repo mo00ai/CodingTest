94d9c93b11575789d2289963305c214256368ff6
AhyunCho <jah112482@gmail.com>
2025-08-24 22:55:57 +0900

    feat : 0824

---
+++ src/backjoon/P10171.java
@@ -0,0 +1,12 @@
+package backjoon;
+
+public class P10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
