084d2fe561e3c33588c6afb847d22b3805ef066f
AhyunCho <jah112482@gmail.com>
2025-09-25 14:52:12 +0900

    feat : 0925

--- src/programmers/lv2/P006.java
+++ src/programmers/lv2/P006.java
@@ -5,7 +5,9 @@ public class P006 {
 
 	public boolean solution(String s) {
 
-		boolean answer = false;
+		if(!(s.length()==4 || s.length() ==6)) {
+			return false;
+		}
 
 		for(int i=0; i<s.length(); i++) {
 			if(!(s.charAt(i)>=48 && s.charAt(i)<=57)) {
