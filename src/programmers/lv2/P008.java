e49733bd66876f1ddc0fd150a1c10bf163c3af51
AhyunCho <jah112482@gmail.com>
2025-09-29 16:11:11 +0900

    feat : 0929

---
+++ src/programmers/lv2/P008.java
@@ -0,0 +1,21 @@
+package programmers.lv2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//38 직사각형 별
+public class P008 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		for(int i=0; i<b; i++) {
+			for(int j=0; j<a; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
