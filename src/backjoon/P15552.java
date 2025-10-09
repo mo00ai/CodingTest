a26de4133f7ebfd551c1b585e0cd008400c08aeb
AhyunCho <jah112482@gmail.com>
2025-10-09 17:49:06 +0900

    feat : 1009

---
+++ src/backjoon/P15552.java
@@ -0,0 +1,32 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class P15552 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			bw.write((A + B) + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+
+	}
+
+}
