30470499537872403498369fe02b9dc8f59b1bba
AhyunCho <jah112482@gmail.com>
2025-11-15 23:53:20 +0900

    1115

---
+++ src/backjoon/P10807.java
@@ -0,0 +1,29 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class P10807 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int count = sc.nextInt();
+		int[] arr = new int[count];
+
+		for (int i = 0; i < count; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int findingNum = sc.nextInt();
+		int result = 0;
+
+		for (int num : arr) {
+			if (num == findingNum) {
+				result++;
+			}
+		}
+
+		System.out.println(result);
+	}
+
+}
