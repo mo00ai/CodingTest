b538976fa6e53d47a824742eb39b526d34b0f684
AhyunCho <jah112482@gmail.com>
2025-11-22 23:29:42 +0900

    1122

---
+++ src/backjoon/P2562.java
@@ -0,0 +1,25 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class P2562 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int max = 0;
+		int index = 0;
+
+		for (int i = 1; i <= 9; i++) {
+			int num = sc.nextInt();
+			if (num > max) {
+				max = num;
+				index = i;
+			}
+		}
+
+		System.out.println(max);
+		System.out.println(index);
+	}
+
+}
