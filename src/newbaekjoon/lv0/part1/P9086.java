9918f4a6b52e5c096f2f27aa9ce26fa61217a1f8
AhyunCho <jah112482@gmail.com>
2026-03-03 23:51:05 +0900

    0223

---
+++ src/newbaekjoon/lv0/part1/P9086.java
@@ -0,0 +1,21 @@
+package newbaekjoon.lv0.part1;
+
+import java.util.Scanner;
+
+public class P9086 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		sc.nextLine();
+
+		for(int i = 0; i < T; i++) {
+			String str = sc.nextLine();
+			System.out.println(str.charAt(0) + "" + str.charAt(str.length() - 1));
+		}
+
+	}
+
+}
